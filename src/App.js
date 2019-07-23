6f2e58b38d5c869b714bea7d21b63f50e66ba978
Nelson Rodrigues <nelson@waveweb2.com>
2019-07-23 01:04:20 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { genreShaker } from "./Genres";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            randomGenre: genreShaker()
+        };
+    }
+
+    randomize() {
+        this.setState({
+            randomGenre: genreShaker()
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Video Game Shaker</h1>
+
+                <main>
+                    <div className="genre">{this.state.randomGenre}</div>
+                    <div className="randomize">
+                        <button onClick={_ => this.randomize()}>Generate</button>
+                    </div>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Genres.js
@@ -0,0 +1,92 @@
+const GameGenres = [
+    "Action",
+    "Action role-playing",
+    "Action-adventure",
+    "Adventure",
+    "Alternate reality",
+    "Art",
+    "Artillery",
+    "Battle royale",
+    "Beat 'em up",
+    "Bishōjo",
+    "Business simulation",
+    "City-building",
+    "Collectible card",
+    "Combat flight simulation",
+    "Construction and management simulation",
+    "Dating sim",
+    "Dungeon crawl",
+    "Escape the room",
+    "Falling-sand",
+    "Fighting",
+    "First-person shooter",
+    "Girls'",
+    "God",
+    "Government simulation",
+    "Grand Theft Auto clone",
+    "Hack and slash",
+    "Immersive sim",
+    "Incremental",
+    "Interactive film",
+    "Kart racing",
+    "Life simulation",
+    "Light gun shooter",
+    "Massively multiplayer online",
+    "Metroidvania",
+    "MUD",
+    "Multiplayer online battle arena",
+    "Music",
+    "Non-game",
+    "Nonviolent",
+    "Olympic",
+    "Open world",
+    "Otome",
+    "Platform",
+    "Programming",
+    "Puzzle",
+    "Racing",
+    "Real-time strategy",
+    "Real-time tactics",
+    "Rhythm",
+    "Roguelike",
+    "Role-playing",
+    "Shoot 'em up",
+    "Shooter",
+    "Sim racing",
+    "Simulation",
+    "Social simulation",
+    "Space flight simulation",
+    "Sports",
+    "Stealth",
+    "Strategy",
+    "Survival",
+    "Survival horror",
+    "Tactical role-playing",
+    "Tactical shooter",
+    "Third-person shooter",
+    "Time management",
+    "Tower defense",
+    "Turn-based strategy",
+    "Turn-based tactics",
+    "Vehicle simulation",
+    "List of vehicular combat",
+    "Vertically scrolling",
+    "Visual novel",
+    "Wargame"
+];
+
+export function getRandomGenre() {
+    const randomIndex = Math.floor(Math.random() * (GameGenres.length - 1));
+
+    return GameGenres[randomIndex];
+}
+
+export function genreShaker() {
+    const game1 = getRandomGenre();
+    const game2 = getRandomGenre();
+    const game3 = getRandomGenre();
+
+    return `${game1} ${game2} ${game3} game`;
+}
+
+export default GameGenres;
