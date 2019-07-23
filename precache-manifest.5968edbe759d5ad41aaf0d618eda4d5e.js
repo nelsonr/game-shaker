73d36997a7ea3fe1b98d71c998c3f5c99704a467
Nelson Rodrigues <nelson@waveweb2.com>
2019-07-23 01:22:24 +0100

    Updates

---
+++ precache-manifest.5968edbe759d5ad41aaf0d618eda4d5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933dc1dbcb34fe9c1c67e8132d0af93c",
+    "url": "/game-shaker/index.html"
+  },
+  {
+    "revision": "2fccafd38fae3befed22",
+    "url": "/game-shaker/static/css/main.8977dd2c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/game-shaker/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2fccafd38fae3befed22",
+    "url": "/game-shaker/static/js/main.dc9b9dc9.chunk.js"
+  },
+  {
+    "revision": "5cf5b045e472a70bcbb3",
+    "url": "/game-shaker/static/js/runtime~main.44a53e1f.js"
+  }
+]);
